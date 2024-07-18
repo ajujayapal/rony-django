c45e2b98eac7c693a884e95d769e788f045ecb48
ajujayapal <aju.jayapal@gmail.com>
2024-07-18 01:08:22 -0500

    Jokes working

--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import pyjokes
 
+def get_joke():
+    return  pyjokes.get_joke()
 
 def index(request):
-    return render(request, 'website/index.html')
+    context = {'joke': get_joke()}
+    return render(request, 'website/index.html', context=context)
